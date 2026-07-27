1396e84ad82102027ca792dac850347c894d6255
Kirtan Gajjar <w.kgajjar@gmail.com>
2026-07-27 16:39:48 +0530

    Initialize project with TypeScript setup, environment configuration, and basic structure

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// index.js for my understanding. (to differentiate between ts and js in function declarations)
+function greet(name) {
+  return `Hello ${name}`;
+}
+
+console.log(greet("Kirtan"));
+
