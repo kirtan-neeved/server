d5fec460065565000cafc4d4f303bcb14ff07b88
Kirtan Gajjar <w.kgajjar@gmail.com>
2026-07-28 14:04:13 +0530

    feat: add generateRandomId function

---
+++ src/utils/generateRandomId.ts
@@ -0,0 +1,5 @@
+// For generating random id based on date to assign every task uniquely
+
+export function generateId(): string {
+  return Date.now().toString(36) + Math.random().toString(36).substring(2, 7);
+}
