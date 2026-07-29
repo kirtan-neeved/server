666d8d8d6386b1cbf1e5a71e7f7fb4881433bacd
Kirtan Gajjar <w.kgajjar@gmail.com>
2026-07-29 11:51:33 +0530

    feat: add asyncHandler that wraps function and execute function using promise

---
+++ src/utils/asyncHandler.ts
@@ -0,0 +1,7 @@
+import type { Request, Response, NextFunction } from "express"
+
+export const asyncHandler = (func: Function) => {
+    return (req: Request, res: Response, next: NextFunction) => {
+        Promise.resolve(func(req, res, next)).catch((err) => next(err))
+    }
+}
